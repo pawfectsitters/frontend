e952a248e5ed60ccdde34bda284c1cc9b735d0ad
Michele <urano.michele@gmail.com>
2018-11-26 20:25:02 +0400

    Better

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express'); // Import express into the file
 const mongoose = require('mongoose'); // Import mongoose into the file
 const bodyParser = require('body-parser'); // Import body-parser
 const cors = require('cors'); // Import CORS (Cross-Origin Resource-Sharing)
-const passport = require('passport');
+const passport = require('passport'); // This is used to authenticate the user after password decryption
 
 const routes = require('./routes/users');
 
@@ -25,7 +25,8 @@ mongoose.connection.once('open', ()=>{ //
     console.log("Database is connected");
 });
 
-server.listen(process.env.PORT, ()=>{
+server.listen(process.env.PORT, ()=>{ //GLOBAL VARIABLE, like Port, across all files, like domain, mongodb url, etc.
+    // we can create a variable when noding index.js like PORT=3333 node index.js
     console.log('the port is', process.env.PORT);
     console.log("Server is running at http://localhost:"+process.env.PORT)
 });
