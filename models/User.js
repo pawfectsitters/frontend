4f666332090b776c40bb64707a0ecff95c9a3b66
Michele <urano.michele@gmail.com>
2018-12-15 11:23:30 +0400

    user schema

--- models/User.js
+++ models/User.js
@@ -4,12 +4,11 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     name: {type: String, required: true},
-    password: {type: String, required: true},
     email: {type: String, required: true},
-    avatar: {type: String, required: false},
-    city: {type: String, required: false},
+    password: {type: String, required: true},
+    location: {type: String, required: true},
+    pettype: {type: String, required: true},
     date: {type: Date, default: Date.now},
-    metaInformation: {type: Array, required:false}
 });
 
 
